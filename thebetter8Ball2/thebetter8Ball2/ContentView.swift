4b6a497548bddc69edd834a77ad9468346a53d4a
samisham <143005992+samisham@users.noreply.github.com>
2023-09-29 10:46:41 -0600

    swift file
    forgot the swift file :/

--- thebetter8Ball2/thebetter8Ball2/ContentView.swift
+++ thebetter8Ball2/thebetter8Ball2/ContentView.swift
@@ -8,12 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var prediction = "tap to predict"
+    func getPrediction() -> String {
+        let choice = Int.random(in: 1...6)
+        switch choice{
+        case 1:
+            return "get to work"
+        case 2:
+            return "are you sure you want to wear that?"
+        case 3:
+            return "did you do the laundry "
+        case 4:
+            return "are you sure you should be eating that?"
+        case 5:
+            return "learn to love yourself"
+        case 6:
+            return "go hit the gym"
+        default:
+            return "mow the lawn"
+        }
+       
+    }
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Image("magic8ball")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            Text("\(prediction)")
+                .background(Color .white.opacity(0.5).cornerRadius(5))
+                .padding()
+                .font(.system(size: 40))
+             
+                
+        }
+        .onTapGesture {
+            prediction = getPrediction()
         }
         .padding()
     }
